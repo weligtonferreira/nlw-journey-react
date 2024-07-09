843897b244c9dbb756c6b97446c8b69b4610f0f9
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-07-09 12:33:04 -0300

    :sparkles: feat: add an input to select trip guests

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,23 @@
-import { ArrowRight, Calendar, MapPin } from 'lucide-react';
+import { useState } from 'react';
+import {
+  ArrowRight,
+  Calendar,
+  MapPin,
+  Settings2,
+  UserRoundPlus,
+} from 'lucide-react';
 
 export function App() {
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
+
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true);
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false);
+  }
+
   return (
     <div className='h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center'>
       <div className='max-w-3xl w-full px-6 text-center space-y-10'>
@@ -11,31 +28,68 @@ export function App() {
           </p>
         </div>
 
-        <div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3'>
-          <div className='flex items-center gap-2 flex-1'>
-            <MapPin className='size-5 text-zinc-400' />
-            <input
-              type='text'
-              placeholder='Para onde você vai?'
-              className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'
-            />
+        <div className='space-y-4'>
+          <div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3'>
+            <div className='flex items-center gap-2 flex-1'>
+              <MapPin className='size-5 text-zinc-400' />
+              <input
+                type='text'
+                disabled={isGuestsInputOpen}
+                placeholder='Para onde você vai?'
+                className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'
+              />
+            </div>
+
+            <div className='flex items-center gap-2'>
+              <Calendar className='size-5 text-zinc-400' />
+              <input
+                type='text'
+                disabled={isGuestsInputOpen}
+                placeholder='Quando?'
+                className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none'
+              />
+            </div>
+
+            <div className='w-px h-6 bg-zinc-800'></div>
+
+            {isGuestsInputOpen ? (
+              <button
+                onClick={closeGuestsInput}
+                className='flex items-center gap-2 bg-zinc-800 text-zinc-200 font-medium rounded-lg px-5 py-2 hover:bg-zinc-700 transition-colors duration-300'
+              >
+                Alterar local/data
+                <Settings2 className='size-5' />
+              </button>
+            ) : (
+              <button
+                onClick={openGuestsInput}
+                className='flex items-center gap-2 bg-lime-300 text-lime-950 font-medium rounded-lg px-5 py-2 hover:bg-lime-400 transition-colors duration-300'
+              >
+                Continuar
+                <ArrowRight className='size-5' />
+              </button>
+            )}
           </div>
 
-          <div className='flex items-center gap-2'>
-            <Calendar className='size-5 text-zinc-400' />
-            <input
-              type='text'
-              placeholder='Quando?'
-              className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none'
-            />
-          </div>
+          {isGuestsInputOpen && (
+            <div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3'>
+              <div className='flex items-center gap-2 flex-1'>
+                <UserRoundPlus className='size-5 text-zinc-400' />
+                <input
+                  type='text'
+                  placeholder='Quem estará na viagem?'
+                  className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'
+                />
+              </div>
 
-          <div className='w-px h-6 bg-zinc-800'></div>
+              <div className='w-px h-6 bg-zinc-800'></div>
 
-          <button className='flex items-center gap-2 bg-lime-300 text-lime-950 font-medium rounded-lg px-5 py-2 hover:bg-lime-400 transition-colors duration-300'>
-            Continuar
-            <ArrowRight className='size-5' />
-          </button>
+              <button className='flex items-center gap-2 bg-lime-300 text-lime-950 font-medium rounded-lg px-5 py-2 hover:bg-lime-400 transition-colors duration-300'>
+                Confirmar viagem
+                <ArrowRight className='size-5' />
+              </button>
+            </div>
+          )}
         </div>
 
         <p className='text-small text-zinc-500'>
