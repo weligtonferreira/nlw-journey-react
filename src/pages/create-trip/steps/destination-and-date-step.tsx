ba51efcbd2a7989639476ddee090271066bbd305
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-07-10 11:45:14 -0300

    :recycle: refactor: separate the trip creation page code into different components

---
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -0,0 +1,57 @@
+import { ArrowRight, Calendar, MapPin, Settings2 } from 'lucide-react';
+
+interface DestinationAndDateStepProps {
+  isGuestsInputOpen: boolean;
+  openGuestsInput: () => void;
+  closeGuestsInput: () => void;
+}
+
+export function DestinationAndDateStep({
+  isGuestsInputOpen,
+  openGuestsInput,
+  closeGuestsInput,
+}: DestinationAndDateStepProps) {
+  return (
+    <div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3'>
+      <div className='flex items-center gap-2 flex-1'>
+        <MapPin className='size-5 text-zinc-400' />
+        <input
+          type='text'
+          disabled={isGuestsInputOpen}
+          placeholder='Para onde você vai?'
+          className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1'
+        />
+      </div>
+
+      <div className='flex items-center gap-2'>
+        <Calendar className='size-5 text-zinc-400' />
+        <input
+          type='text'
+          disabled={isGuestsInputOpen}
+          placeholder='Quando?'
+          className='bg-transparent text-lg placeholder-zinc-400 w-40 outline-none'
+        />
+      </div>
+
+      <div className='w-px h-6 bg-zinc-800'></div>
+
+      {isGuestsInputOpen ? (
+        <button
+          onClick={closeGuestsInput}
+          className='flex items-center gap-2 bg-zinc-800 text-zinc-200 font-medium rounded-lg px-5 py-2 hover:bg-zinc-700 transition-colors duration-300'
+        >
+          Alterar local/data
+          <Settings2 className='size-5' />
+        </button>
+      ) : (
+        <button
+          onClick={openGuestsInput}
+          className='flex items-center gap-2 bg-lime-300 text-lime-950 font-medium rounded-lg px-5 py-2 hover:bg-lime-400 transition-colors duration-300'
+        >
+          Continuar
+          <ArrowRight className='size-5' />
+        </button>
+      )}
+    </div>
+  );
+}
