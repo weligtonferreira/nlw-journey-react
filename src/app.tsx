363cf62f248a17354d2c8e2813276ef60cee1e12
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-07-10 10:52:14 -0300

    :sparkles: feat: show the number of invited emails on the GuestsModal

--- src/app.tsx
+++ src/app.tsx
@@ -118,9 +118,15 @@ export function App() {
               >
                 <UserRoundPlus className='size-5 text-zinc-400' />
 
-                <span className='text-zinc-400 text-lg flex-1'>
-                  Quem estará na viagem?
-                </span>
+                {emailsToInvite.length > 0 ? (
+                  <span className='text-zinc-100 text-lg flex-1'>
+                    {emailsToInvite.length} pessoa(s) convidada(s)
+                  </span>
+                ) : (
+                  <span className='text-zinc-400 text-lg flex-1'>
+                    Quem estará na viagem?
+                  </span>
+                )}
               </button>
 
               <div className='w-px h-6 bg-zinc-800'></div>
